bcfbb2b3fb9b18ceb642854fbd9cf138e273c5da
unknown <you@example.com>
2023-01-29 16:21:04 +0300

    chat

--- project/src/components/contact/index.jsx
+++ project/src/components/contact/index.jsx
@@ -1,18 +1,90 @@
-import './Contact.css'
-function Contact() {
-    return <div>
-        <br/>
-        <br/>
-        <form>
+import "./Contact.css";
 
-        <label for="fname">First name:</label><br/>
-        <input type="text" id="fname" name="fname" placeholder="First Name"></input><br/>
-        <label for="fname">Last name:</label><br/>
-        <input type="text" id="lname" name="lname" placeholder="Second Name"></input><br/>
-        <label for="fname">Message:</label><br/>
-        <input type="text" id="message" name="message" placeholder="Message"></input><br/><br/>
-        <button>Submit</button>
-        </form>
-    </div>
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import { Key, Send } from '@mui/icons-material/';
+import { useState } from "react";
+import { useRef } from "react";
+
+
+function Contact() {
+
+const [myMessage , setMyMessages] = useState([])
+const [newMessage , setNewMessages] = useState("")
+
+
+const sendMessage =(e) =>{
+  e.preventDefault()
+
+  console.log("message : " , newMessage) 
+
+  setMyMessages ([...myMessage , newMessage])
+  // e.target.reset()
+}
+
+
+
+
+
+
+
+  return (
+    <Box>
+
+      <div className="messagewindow">
+
+
+      <div className='left message'>
+        this is a text message 
+      </div>
+
+      <div className='right message'>
+        this is a text message
+      </div>
+
+      <div className='right message'>
+        this is a text message
+      </div>
+
+      <div className='left message'>
+        this is a text message 
+      </div>
+
+      <div className='left message'>
+        this is a text message 
+      </div>
+
+      <div className='right message'>
+        this is a text message
+      </div>
+
+        {myMessage.map((eachMessage , Key) => {
+          return <div key={Key} className='left message'>
+            {eachMessage}
+        </div>
+        })}
+
+
+
+      </div>
+
+
+<form onSubmit = {sendMessage}>
+
+      <Box  sx={{position:"fixed" , bottom: 0 , width:"100%" , display:"flex" }}>
+
+        <TextField onChange={(e) => {setNewMessages (e.target.value)}} sx ={{width:"100%"}} id="outlined-basic" label="Outlined" placeholder='Type Your Message here' variant="outlined" />
+        
+        <Box >
+        <IconButton  aria-label="send" size="large" >
+          <Send fontSize="inherit" />
+        </IconButton>
+        </Box>
+      </Box>
+</form>
+    </Box>
+  );
 }
 export default Contact
--- project/src/index.js
+++ project/src/index.js
@@ -4,15 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-// import bootstrap from 'bootstrap'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+
+
+// const theme = createTheme({
+// //   palette: {
+// //     primary: {
+// //       main: red[500],
+// //     },
+// //   },
+// // });
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* <ThemeProvider theme={theme}> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    {/* </ThemeProvider> */}
   </React.StrictMode>
 );
 
